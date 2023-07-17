158f245887f9a4c40f72520558cdc55f0e16ae17
atman-33 <41929192+atman-33@users.noreply.github.com>
2023-07-17 16:45:04 +0900

    add prismjs sample

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,39 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { SharedModule } from './services/shared.module';
+import { PrismService } from './services/prism.service';
+import * as HtmlUtils from './services/html-utils'
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
-  `,
+  imports: [CommonModule, SharedModule],
+  templateUrl: './main.html',
+  styleUrls: ['./main.scss']
 })
-export class App {
+export class App implements OnInit,AfterViewChecked {
   name = 'Angular';
+
+  highlighted = false;
+
+  code = `
+  <p>Here's how you can customize the appearance of an Angular Material toggle button. Although the method is unique, I'll explain it in this article.</p>\n<h2>1. How to use Angular Material button-toggle</h2>\n<h3>Install Angular Material</h3>\n<pre><code class=\"language-bash\">ng add @angular/material\n</code></pre>\n<h3>Import Angular Material button-toggle to Module file</h3>\n<pre><code class=\"language-ts\">import {MatButtonToggleModule} from '@angular/material/button-toggle'\n</code></pre>\n<h3>Html</h3>\n<pre><code class=\"language-html\">&lt;mat-button-toggle [aria-label]=&quot;alertsEnabled ? 'Disable alerts' : 'Enable alerts'&quot;&gt;\n  &lt;mat-icon&gt;notifications&lt;/mat-icon&gt;\n&lt;/mat-button-toggle&gt;\n</code></pre>\n<p>For more details, refer to the official documentation:<br>\n<a href=\"https://material.angular.io/components/button-toggle\">Angular Material Official Documentation</a></p>
+`;
+
+  constructor(private readonly prismService: PrismService) {}
+
+  ngOnInit(){
+      this.code = HtmlUtils.addClassToHtml(this.code, 'line-numbers', 'pre');
+  }
+
+  ngAfterViewChecked() {
+    if (!this.highlighted && this.code) {
+      this.prismService.highlightAll();
+      this.highlighted = true;
+    }
+  }
 }
 
 bootstrapApplication(App);
